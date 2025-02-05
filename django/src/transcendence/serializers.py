4a8441ceb150069f5654ea8c6d91ca4ac5eddca3
TheoGerardin <theogerardin16@gmail.com>
2025-02-05 22:47:06 +0100

    csrf

--- django/src/transcendence/serializers.py
+++ django/src/transcendence/serializers.py
@@ -1,7 +1,22 @@
 from rest_framework import serializers
 from .models import User
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'nickname', 'elo', 'email', 'password']
+        fields = ['id', 'nickname', 'elo', 'email', 'password', 'is_active']
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        email = attrs.get("email")
+        password = attrs.get("password")
+
+        user = User.objects.filter(email=email).first()
+        if user and user.check_password(password):
+            if not user.is_active:
+                raise serializers.ValidationError("User account is disabled.")
+            attrs["username"] = user.email
+            return super().validate(attrs)
+        else:
+            raise serializers.ValidationError("Invalid email or password")
--- django/src/transcendence/theo_views.py
+++ django/src/transcendence/theo_views.py
@@ -1,9 +1,17 @@
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializers import CustomTokenObtainPairSerializer
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+
+@method_decorator(csrf_exempt, name='dispatch')
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
 
 class ProtectedView(APIView):
-    permission_classes = [IsAuthenticated]  # cette vue nécessite une authentification JWT
+    permission_classes = [IsAuthenticated]
 
     def get(self, request):
         return Response({"message": "Vous avez accès à cette vue protégée!"})
