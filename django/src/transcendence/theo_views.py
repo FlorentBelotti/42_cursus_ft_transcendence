10ea016756a354532f8c0734233c00f0472ff0d4
TheoGerardin <theogerardin16@gmail.com>
2025-02-06 18:52:44 +0100

    Update theo_views.py

--- django/src/transcendence/theo_views.py
+++ django/src/transcendence/theo_views.py
@@ -1,4 +1,3 @@
-# filepath: /home/theo/Documents/GitHub/ft_transcendence/django/src/transcendence/theo_views.py
 from django.contrib.auth import get_user_model
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
