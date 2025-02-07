ee3ebd96f50d5542bbe1c93bee9078a56e6874cc
TheoGerardin <theogerardin16@gmail.com>
2025-02-07 18:37:39 +0100

    test avant merge

--- django/src/transcendence/middleware.py
+++ django/src/transcendence/middleware.py
@@ -1,4 +1,3 @@
-# filepath: /home/theo/Documents/GitHub/ft_transcendence/django/src/transcendence/middleware.py
 from django.utils.deprecation import MiddlewareMixin
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.exceptions import AuthenticationFailed
@@ -17,23 +16,23 @@ class AccessControlMiddleware(MiddlewareMixin):
             '/api/token-ranked/',
         ]
 
-        if request.path in public_urls:
+        if request.path not in public_urls:
             return
-
-        jwt_authenticator = JWTAuthentication()
-        try:
-            auth_result = jwt_authenticator.authenticate(request)
-            if auth_result is None:
-                raise AuthenticationFailed('Invalid token')
-            user, token = auth_result
-            request.user = user
-            request.token = token
-        except AuthenticationFailed:
-            return JsonResponse({'detail': 'Invalid token'}, status=401)
-
-        if request.path in rank_0_urls:
-            if request.token.get('rank') != 0:
-                return JsonResponse({'detail': 'Access denied'}, status=403)
         else:
-            if request.token.get('rank') not in [1, 2]:
-                return JsonResponse({'detail': 'Access denied'}, status=403)
+            jwt_authenticator = JWTAuthentication()
+            try:
+                auth_result = jwt_authenticator.authenticate(request)
+                if auth_result is None:
+                    raise AuthenticationFailed('Invalid token')
+                user, token = auth_result
+                request.user = user
+                request.token = token
+            except AuthenticationFailed:
+                return JsonResponse({'detail': 'Invalid token3'}, status=401)
+
+            if request.path in rank_0_urls:
+                if request.token.get('rank') != 0:
+                    return JsonResponse({'detail': 'Access denied'}, status=403)
+            else:
+                if request.token.get('rank') not in [1, 2]:
+                    return JsonResponse({'detail': 'Access denied'}, status=403)
