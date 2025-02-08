cff6259365e22ac9ce5e60934adefc98ca21d7fa
TheoGerardin <theogerardin16@gmail.com>
2025-02-08 16:22:19 +0100

    -- token-ranked

--- django/src/transcendence/urls.py
+++ django/src/transcendence/urls.py
@@ -31,7 +31,6 @@ urlpatterns = [
     path('api/users/<int:pk>/update/', user_controller.user_update, name='user_update'),
     path('api/users/<int:pk>/delete/', user_controller.user_delete, name='user_delete'),
     path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token-ranked/', CustomTokenObtainPairViewRank.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/send-verification-code/', SendVerificationCodeView.as_view(), name='send_verification_code'),
     path('api/verify-code/', VerifyCodeView.as_view(), name='verify_code'),
