6612da2df17c26b5cd244677ec599eb60fc25d21
Romain Lambert <lambertromain66270@gmail.com>
2025-03-28 13:51:15 +0100

    login

--- django/src/static/js/login.js
+++ django/src/static/js/login.js
@@ -1,31 +1,30 @@
 function loginFormEvent() {
-    document.getElementById("loginForm").addEventListener("submit", async function (event){
+    document.getElementById("loginForm").addEventListener("submit", async function (event) {
         event.preventDefault();
         let loginFormData = new FormData(event.target);
         let response = await fetch('/login/', {
-            method:"post",
-            body:loginFormData,
+            method: "post",
+            body: loginFormData,
             headers: {
                 "X-CSRFToken": document.querySelector("[name=csrfmiddlewaretoken]").value
             }
-        })
-        let data = await response.json()
+        });
+        let data = await response.json();
         if ('success' in data) {
             console.log('Success:', "Redirecting to double authentication");
             localStorage.setItem('user_id', data.user_id);
-            let verifyCodeUrl = `/verify_code/${data.user_id}/`;
-            window.loadContent(verifyCodeUrl);
-        }
-        else if ('error' in data) {
+            // Afficher la modale au lieu de charger une page
+            document.getElementById('verifyModal').style.display = 'block';
+        } else if ('error' in data) {
             console.log('Error:', "Wrong username or password");
-            window.loadContent('/login/');
+            const errorDiv = document.getElementById("errorMessage");
+            errorDiv.textContent = data.error;
+            errorDiv.style.display = "block";
             setTimeout(() => {
-                const errorDiv = document.getElementById("errorMessage");
-                errorDiv.textContent = data.error;
-                errorDiv.style.display = "block";
-            }, 100);
+                errorDiv.style.display = "none";
+            }, 3000); 
         }
-    })
+    });
 }
 
 document.addEventListener("DOMContentLoaded", function () {
